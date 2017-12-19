5cd8296a68029f88cd0853fa347fbfbe1db2a73f
swlxm <swlxm2002@gmail.com>
2017-12-19 10:27:22 +0800

    updated

---
+++ hello-appium/src/main/java/net/automation/mobile/scripts/settings/android/SettingsConstants.java
@@ -0,0 +1,9 @@
+package net.automation.mobile.scripts.settings.android;
+
+import net.automation.mobile.util.AppiumConstants;
+
+public interface SettingsConstants extends AppiumConstants {
+
+	public static final String PACKAGE_NAME = "com.android.settings";
+	public static final String ACTIVITY_NAME = ".Settings";
+}
